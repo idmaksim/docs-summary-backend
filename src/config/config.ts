2da9993e6faed0a6da575140af5097fb8bb5913a
idmaksim <dmaksim@tuta.io>
2024-11-18 23:45:27 +0300

    #1 Add prisma, validation libs, moved files,

---
+++ src/config/config.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  PORT: parseInt(process.env.PORT, 10) || 3000,
+  DATABASE_URL: process.env.DATABASE_URL,
+  ACCESS_SECRET: process.env.ACCESS_SECRET,
+  REFRESH_SECRET: process.env.REFRESH_SECRET,
+  S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
+  S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+  S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+});
