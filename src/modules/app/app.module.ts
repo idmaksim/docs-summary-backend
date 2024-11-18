40dfa1c4fa30d2d22587a8f14f1b97cb5c632e8f
idmaksim <dmaksim@tuta.io>
2024-11-19 00:15:03 +0300

    #2 Implement authorization, make migrations for user table

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,9 +2,15 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
 import config from 'src/config/config';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [config] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
