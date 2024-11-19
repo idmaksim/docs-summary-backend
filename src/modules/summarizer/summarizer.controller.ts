e9d6cc3788870fd4176076962d982df09749f65a
idmaksim <dmaksim@tuta.io>
2024-11-19 03:15:08 +0300

    #3 Add guard to summarization

--- src/modules/summarizer/summarizer.controller.ts
+++ src/modules/summarizer/summarizer.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { SummarizerGateway } from './summarizer.gateway';
@@ -10,6 +11,7 @@ import { Express } from 'express';
 import { ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { SummarizeTextDto } from './dto/summarize-text.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('summarizer')
 @ApiTags('Summarizer')
@@ -17,12 +19,14 @@ export class SummarizerController {
   constructor(private readonly summarizerGateway: SummarizerGateway) {}
 
   @Post('file')
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('file'))
   async summarizeFromFile(@UploadedFile() file: Express.Multer.File) {
     return this.summarizerGateway.summarizeFromFile(file);
   }
 
   @Post('text')
+  @UseGuards(JwtAuthGuard)
   async summarizeFromText(@Body() body: SummarizeTextDto) {
     return this.summarizerGateway.summarizeFromText(body.text);
   }
--- src/modules/summarizer/summarizer.module.ts
+++ src/modules/summarizer/summarizer.module.ts
@@ -4,9 +4,10 @@ import { SummarizerController } from './summarizer.controller';
 import { SummarizerGateway } from './summarizer.gateway';
 import { DocxService } from './services/docx.service';
 import { ModelModule } from '../model/model.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [ModelModule],
+  imports: [ModelModule, UsersModule],
   controllers: [SummarizerController],
   providers: [SummarizerGateway, DocxService],
 })
