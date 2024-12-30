d629519626f0e79adbb613be6549babcdc171e92
idmaksim <dmaksim@tuta.io>
2024-12-31 01:21:54 +0300

    Refactor user module to remove PrismaService and replace with PrismaModule. Update import paths for PrismaService in users.repository.ts.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,12 +3,12 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { PasswordModule } from '../password/password.module';
-import { PrismaService } from 'src/common/services/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PasswordModule],
+  imports: [PasswordModule, PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}
--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/common/services/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { SignUpDto } from '../auth/dto/sign-up.dto';
 
 @Injectable()
