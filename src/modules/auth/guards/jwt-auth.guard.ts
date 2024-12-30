c1bac1976d934f24e41e30228bfc5e907732ee53
idmaksim <dmaksim@tuta.io>
2024-12-31 01:52:16 +0300

    Refactor JwtAuthGuard to simplify implementation by removing unnecessary error handling and imports.

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,27 +1,3 @@
-import {
-  HttpException,
-  HttpStatus,
-  UnauthorizedException,
-} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { JsonWebTokenError } from 'jsonwebtoken';
 
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor() {
-    super();
-  }
-  handleRequest(err: any, user: any, info: any, context: any, status: any) {
-    try {
-      if (info instanceof JsonWebTokenError) {
-        throw new UnauthorizedException();
-      }
-      return super.handleRequest(err, user, info, context, status);
-    } catch (error) {
-      throw new HttpException(
-        error.message,
-        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
-        { cause: error },
-      );
-    }
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
